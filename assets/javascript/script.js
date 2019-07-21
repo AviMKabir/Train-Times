7f85b99fb8110d2b3a54edf197e5ffb631571a5d
Avi M Kabir <avimishkakabir@gmail.com>
2019-07-21 15:40:44 -0400

    finished layout and initialized bootstrap

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBWWlRRdKytiJIk28q5pMLhTIVVhyWv2XM",
+    authDomain: "train-times-54115.firebaseapp.com",
+    databaseURL: "https://train-times-54115.firebaseio.com",
+    projectId: "train-times-54115",
+    storageBucket: "",
+    messagingSenderId: "611518358397",
+    appId: "1:611518358397:web:872c25ef9796b6a2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
